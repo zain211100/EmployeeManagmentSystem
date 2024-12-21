41986d01b9a793791ed5db051807870409663e85
Full-Stack-Gruppe-3 <245632@usn.no>
2024-12-21 19:37:18 +0100

    .

---
+++ BaseLibrary/Entities/Relationship.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BaseLibrary.Entities
+{
+    public class Relationship
+    {
+        //Relationship : One To Many
+        [JsonIgnore]
+
+        public List<Employee>? Employees { get; set; }
+    }
+}
---
+++ ServerLibrary/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ServerLibrary
+{
+    public class Class1
+    {
+
+    }
+}
